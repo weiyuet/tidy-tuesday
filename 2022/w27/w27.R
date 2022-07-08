75d8e6141f8ec5349f2d63a1a33749ed7b784121
weiyuet <weiyuet@gmail.com>
2022-07-09 06:32:10 +0800

    Updated w27

--- 2022/w27/w27.R
+++ 2022/w27/w27.R
@@ -9,7 +9,7 @@ rent <- read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/
 rent_small <- rent %>% 
   drop_na() %>% 
   select(year, nhood, city, price, beds) %>% 
-  filter(city == "cupertino" | city == "san francisco") %>% 
+  filter(city == "oakland" | city == "san francisco" | city == "san jose") %>% 
   arrange(year)
 
 # Prelim plot
@@ -19,15 +19,15 @@ rent_small %>%
   geom_jitter() +
   geom_smooth() +
   facet_wrap(~city) +
-  scale_y_log10(labels = label_dollar(big.mark = ",", accuracy = 1)) +
+  scale_y_log10(labels = label_dollar(prefix = "USD", big.mark = ",", accuracy = 1)) +
   scale_x_continuous() +
   theme_classic() +
   theme(legend.position = "none",
-        axis.text.y = element_text(angle = 35)) +
+        axis.text.y = element_text(angle = 90)) +
   scale_colour_brewer(type = "qual", palette = 6) +
   labs(x = "", y = "",
-       title = "Rents",
-       caption = "#TidyTuesday")
+       title = "Rents in the Bay Area - Core Cities",
+       caption = "Source: Pennington, Kate (2018). Bay Area Craigslist Rental Housing Posts, 2000-2018.\nChart: @weiyuet #TidyTuesday")
 
-ggsave("2022/w27/rent-cupertino-frisco.png", width = 6, height = 6)
+ggsave("2022/w27/rent-cupertino-frisco.png", width = 9, height = 6)
   
