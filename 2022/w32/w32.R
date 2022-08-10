6ed61c478fcbbcfbde3213fa00150408831c2037
weiyuet <weiyuet@gmail.com>
2022-08-10 08:11:34 +0800

    Edited w32

--- 2022/w32/w32.R
+++ 2022/w32/w32.R
@@ -6,8 +6,15 @@ library(scales)
 wheels <- read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-08-09/wheels.csv")
 
 # Prelim plot
+# Relationship between ride duration and height
 wheels %>% 
-  ggplot(aes(x = ride_duration_minutes, y = height)) +
+  mutate(height_metres = height * 0.3048) %>% 
+  ggplot(aes(x = ride_duration_minutes, y = height_metres)) +
   geom_point() +
   geom_smooth() +
-  theme_classic()
+  theme_classic() +
+  labs(x = "Ride duration in minutes", y = "Height in metres",
+       caption = "Source: Emil Hvitfeldt\n#TidyTuesday")
+
+# Save png
+ggsave("2022/w32/ride-duration-and-height.png", width = 6, height = 4)
