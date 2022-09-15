c24b2faa2706e018fccc8b3bc7a87001580b5936
weiyuet <weiyuet@gmail.com>
2022-09-15 18:32:11 +0800

    Edited w37 plots

--- 2022/w32/w32.R
+++ 2022/w32/w32.R
@@ -7,33 +7,40 @@ wheels <- read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesda
 
 # Plot
 # Height of ferris wheels and ride duration
-wheels %>% 
-  mutate(height_metres = height * 0.3048) %>% 
+wheels %>%
+  mutate(height_metres = height * 0.3048) %>%
   ggplot(aes(x = ride_duration_minutes, y = height_metres)) +
   geom_point() +
-  geom_smooth(colour = '#2086AE') +
+  geom_smooth(colour = "#3A5FCD") +
+  scale_x_continuous(expand = c(0, 0)) +
   theme_classic() +
-  labs(x = "Ride duration in minutes", y = "Height in metres",
-       title = "Height of Ferris Wheels vs. Ride Duration",
-       caption = "Source: Emil Hvitfeldt\nGraphic: @weiyuet #TidyTuesday")
+  labs(
+    x = "Ride duration in minutes", y = "Height in metres",
+    title = "Height of Ferris Wheels vs. Ride Duration",
+    caption = "Source: Emil Hvitfeldt\nGraphic: @weiyuet #TidyTuesday"
+  )
 
 # Save png
 ggsave("2022/w32/height-and-ride-duration.png", width = 6, height = 4)
 
 # Height of ferris wheels and date of open
-wheels %>% 
-  select(height, opened, country, number_of_cabins) %>% 
+wheels %>%
+  select(height, opened, country, number_of_cabins) %>%
   ggplot(aes(x = opened, y = height, size = number_of_cabins)) +
   geom_point() +
   scale_x_date() +
-  scale_y_continuous(limits = c(100, 700),
-                     breaks = seq(100, 700, 50)) +
+  scale_y_continuous(
+    limits = c(100, 700),
+    breaks = seq(100, 700, 50)
+  ) +
   theme_classic() +
   theme(legend.position = "bottom") +
-  labs(x = "Year of open", y = "Height in metres",
-       size = "Number of cabins",
-       title = "Height of Ferris Wheels vs. Year of Open",
-       caption = "Source: Emil Hvitfeldt\nGraphic: @weiyuet #TidyTuesday")
+  labs(
+    x = "Year of open", y = "Height in metres",
+    size = "Number of cabins",
+    title = "Height of Ferris Wheels vs. Year of Open",
+    caption = "Source: Emil Hvitfeldt\nGraphic: @weiyuet #TidyTuesday"
+  )
 
 # Save png
 ggsave("2022/w32/height-and-date-of-open.png", width = 6, height = 6)
--- 2022/w37/w37.R
+++ 2022/w37/w37.R
@@ -4,46 +4,58 @@ library(skimr)
 library(scales)
 
 # Load data
-bigfoot <- read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-09-13/bigfoot.csv')
+bigfoot <- read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-09-13/bigfoot.csv")
 
 # Explore data
 skim(bigfoot) %>% summary()
 
-bigfoot %>% select_if(is_numeric) %>% 
+bigfoot %>%
+  select_if(is_numeric) %>%
   skim()
 
 # Plot Bigfoot sightings by season
-bigfoot %>% 
-  count(season) %>% 
-  mutate(season = fct_reorder(season, n)) %>% 
+bigfoot %>%
+  count(season) %>%
+  mutate(season = fct_reorder(season, n)) %>%
   ggplot(aes(x = season, y = n)) +
   geom_col(colour = "gray10", fill = "gray35") +
+  geom_label(aes(x = season, y = n, label = round(n, 0)),
+    size = 3
+  ) +
   coord_flip() +
-  scale_y_continuous(labels = label_number(big.mark = ","),
-                     breaks = seq(0, 2000, 500),
-                     limits = c(0, 2000),
-                     expand = c(0, 0)) +
+  scale_y_continuous(
+    labels = label_number(big.mark = ","),
+    breaks = seq(0, 2000, 500),
+    limits = c(0, 2000),
+    expand = c(0, 0)
+  ) +
   theme_classic() +
-  labs(x = "", y = "",
-       title = "Bigfoot Sightings by Season",
-       caption = "Source: Data.World #TidyTuesday")
+  labs(
+    x = "", y = "",
+    title = "Bigfoot Sightings by Season",
+    caption = "Source: Data.World #TidyTuesday 2022 w37\n Graphic: @weiyuet"
+  )
 
-#Save png
+# Save png
 ggsave("2022/w37/bigfoot-sightings-season.png", width = 6, height = 4.5)
 
 # Plot Bigfoot sightings by state
-bigfoot %>% 
-  count(state) %>% 
-  mutate(state = fct_reorder(state, n)) %>% 
+bigfoot %>%
+  count(state) %>%
+  mutate(state = fct_reorder(state, n)) %>%
   ggplot(aes(x = state, y = n)) +
   geom_col(colour = "gray10", fill = "gray35") +
   coord_flip() +
-  scale_y_continuous(breaks = seq(0, 600, 50),
-                     expand = c(0, 0)) +
+  scale_y_continuous(
+    breaks = seq(0, 600, 50),
+    expand = c(0, 0)
+  ) +
   theme_classic() +
-  labs(x = "", y = "",
-       title = "Bigfoot Sightings by State",
-       caption = "Source: Data.World #TidyTuesday")
+  labs(
+    x = "", y = "",
+    title = "Bigfoot Sightings by State",
+    caption = "Source: Data.World #TidyTuesday 2022 w37\n Graphic: @weiyuet"
+  )
 
 # Save png
 ggsave("2022/w37/bigfoot-sightings-state.png", width = 10, height = 8)
