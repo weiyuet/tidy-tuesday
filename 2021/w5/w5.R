f32bc1901adf3ebc4659d6e9cb0de53e7748ec96
weiyuet <weiyuet@gmail.com>
2022-09-18 15:46:41 +0800

    Updated README

--- 2021/w5/w5.R
+++ 2021/w5/w5.R
@@ -1,16 +1,22 @@
-# Load libraries
+#Setup
 library(tidyverse)
 library(scales)
 
-# Load data
+#Load data
 plastics <- read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-01-26/plastics.csv")
 
-# Wrangle data
+#Wrangle data
 plastics_singapore <- plastics %>% filter(country == "Singapore")
 
-# Prelim plot
-plastics_singapore %>% 
+#Prelim plot
+plastics_singapore %>%
+  mutate(parent_company = fct_reorder(parent_company, grand_total)) %>% 
   ggplot(aes(x = parent_company, y = grand_total)) +
-  geom_point() +
+  geom_col(colour = "gray10", fill = "gray35") +
+  scale_y_continuous(limits = c(0, 40),
+                     expand = c(0, 0)) +
+  coord_flip() +
   theme_classic() +
-  labs(x = "", y = "")
+  labs(x = "", y = "",
+       title = "Plastic Pollution in Singapore",
+       caption = "Source: Break Free from Plastic\n Graphic: @weiyuet #TidyTuesday 2021 w5")
--- 2022/README.md
+++ 2022/README.md
@@ -1,12 +1,12 @@
 ## Archive of 2022 `#TidyTuesdays`
 Week 27
-![Week 27](https://github.com/weiyuet/tidy-tuesday/blob/main/2022/w27/rent-bay-area-core-cities.png)
+![](https://github.com/weiyuet/tidy-tuesday/blob/main/2022/w27/rent-bay-area-core-cities.png)
 
 Week 29
-![Week 29](https://github.com/weiyuet/tidy-tuesday/blob/main/2022/w29/internetuser-asean.png)
+![](https://github.com/weiyuet/tidy-tuesday/blob/main/2022/w29/internetuser-asean.png)
 
 Week 34
-![Week 34](https://github.com/weiyuet/tidy-tuesday/blob/main/2022/w34/process-size-and-transistor-count.png)
+![](https://github.com/weiyuet/tidy-tuesday/blob/main/2022/w34/process-size-and-transistor-count.png)
 
 Week 37
-![Week 37](https://github.com/weiyuet/tidy-tuesday/blob/main/2022/w37/bigfoot-sightings-state.png)
+![](https://github.com/weiyuet/tidy-tuesday/blob/main/2022/w37/bigfoot-sightings-state.png)
