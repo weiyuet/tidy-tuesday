aa4b69797f5f24f0beb47609deff5bbc197d6960
weiyuet <weiyuet@gmail.com>
2022-09-23 18:33:49 +0800

    Added w25, and updated various plots

--- 2022/w25/w25.R
+++ 2022/w25/w25.R
@@ -1,9 +1,41 @@
-#Setup
+# Setup
 library(tidyverse)
 library(skimr)
+library(scales)
+library(ggsci)
+library(glue)
 
-#Load data
-#blackpast <- read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-06-16/blackpast.csv')
-census <- read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-06-16/census.csv')
-#slave_routes <- read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-06-16/slave_routes.csv')
-#african_names <- read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-06-16/african_names.csv')
+# Load data
+# blackpast <- read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-06-16/blackpast.csv')
+census <- read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-06-16/census.csv")
+# slave_routes <- read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-06-16/slave_routes.csv')
+# african_names <- read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-06-16/african_names.csv')
+
+census_tidy <- census %>%
+  filter(region == "USA Total") %>%
+  pivot_longer(., total:black_slaves, names_to = "population_segment", values_to = "count") %>%
+  filter(population_segment == "black" | population_segment == "black_free" | population_segment == "black_slaves") %>%
+  mutate(case_when(population_segment == "black" ~ "Black",
+                   population_segment == "black_free" ~ "Black Free",
+                   population_segment == "black_slaves" ~ "Black Slaves"))
+
+census_tidy %>% 
+  ggplot(aes(x = year, y = count, colour = population_segment)) +
+  geom_line(size = 1.1) +
+  facet_grid(~population_segment) +
+  scale_x_continuous(
+    limits = c(min(census$year), max(census$year)),
+    breaks = seq(min(census$year), max(census$year), 20)
+  ) +
+  scale_y_continuous(labels = label_number(big.mark = ",")) +
+  scale_colour_jco() +
+  theme_classic() +
+  theme(legend.position = "none") +
+  labs(
+    x = "", y = "",
+    colour = "",
+    title = glue("Census of the United States {min(census$year)} - {max(census$year)}"),
+    caption = "Data: U.S. Census Bureau\nGraphic: @weiyuet | #TidyTuesday2022 w25"
+  )
+
+ggsave("2022/w25/us-population-census-1800s.png", width = 8, height = 4.5)
--- 2022/w29/w29.R
+++ 2022/w29/w29.R
@@ -3,71 +3,76 @@ library(tidyverse)
 library(scales)
 
 # Load data
-technology <- read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-07-19/technology.csv')
+technology <- read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-07-19/technology.csv")
 
-#Wrangle data
+# Wrangle data
 # BCG vaccinations in 10 ASEAN member nations
-bcg_asean <- technology %>% 
-  filter(variable == "BCG") %>% 
-  filter(iso3c == "SGP" | iso3c == "MYS" | iso3c == "IDN" | 
-           iso3c == "THA" | iso3c == "KHM" | iso3c == "LAO" | iso3c == "BRN" |
-           iso3c == "MMR" | iso3c == "PHL" | iso3c == "VNM")
+bcg_asean <- technology %>%
+  filter(variable == "BCG") %>%
+  filter(iso3c == "SGP" | iso3c == "MYS" | iso3c == "IDN" |
+    iso3c == "THA" | iso3c == "KHM" | iso3c == "LAO" | iso3c == "BRN" |
+    iso3c == "MMR" | iso3c == "PHL" | iso3c == "VNM")
 
 # Plot BCG vaccinations in 10 ASEAN member nations
-bcg_asean %>% 
+bcg_asean %>%
   ggplot(aes(x = year, y = value, colour = iso3c)) +
   geom_line(size = 1.05, colour = "gray 35") +
   geom_hline(yintercept = 80, colour = "red", linetype = "dotted") +
   facet_wrap(~iso3c, ncol = 2) +
   theme_classic() +
   theme(legend.position = "none") +
-  labs(x = "", y = "",
-       title = "BCG Vaccinations (10 ASEAN Member Nations)",
-       subtitle = "Percentage of children who received an immunization - horizontal line = 80%",
-       caption = "Source: nber.org \nGraphic: @weiyuet #TidyTuesday 2022 w29")
+  labs(
+    x = "", y = "",
+    title = "BCG Vaccinations (10 ASEAN Member Nations)",
+    subtitle = "Percentage of children who received an immunization - horizontal line = 80%",
+    caption = "Data: nber.org \nGraphic: @weiyuet | #TidyTuesday2022 w29"
+  )
 
 # Save png
 ggsave("2022/w29/bcg-vaccinations-asean.png", width = 6, height = 8)
 
 # Wrangle data
 # Pol3 vaccinations in 10 ASEAN nations
-pol3_asean <- technology %>% 
-  filter(variable == "Pol3") %>% 
-  filter(iso3c == "SGP" | iso3c == "MYS" | iso3c == "IDN" | 
-           iso3c == "THA" | iso3c == "KHM" | iso3c == "LAO" | iso3c == "BRN" |
-           iso3c == "MMR" | iso3c == "PHL" | iso3c == "VNM")
+pol3_asean <- technology %>%
+  filter(variable == "Pol3") %>%
+  filter(iso3c == "SGP" | iso3c == "MYS" | iso3c == "IDN" |
+    iso3c == "THA" | iso3c == "KHM" | iso3c == "LAO" | iso3c == "BRN" |
+    iso3c == "MMR" | iso3c == "PHL" | iso3c == "VNM")
 
 # Plot Pol3 vaccinations in 10 ASEAN member nations
-pol3_asean %>% 
+pol3_asean %>%
   ggplot(aes(x = year, y = value, colour = iso3c)) +
   geom_line(size = 1.05, colour = "gray 35") +
   geom_hline(yintercept = 80, colour = "red", linetype = "dotted") +
   facet_wrap(~iso3c, ncol = 2) +
   theme_classic() +
   theme(legend.position = "none") +
-  labs(x = "", y = "",
-       title = "Polio Vaccinations (10 ASEAN Member Nations)",
-       subtitle = "Percentage of children who received an immunization - horizontal line = 80%",
-       caption = "Source: nber.org \nGraphic: @weiyuet #TidyTuesday 2022 w29")
+  labs(
+    x = "", y = "",
+    title = "Polio Vaccinations (10 ASEAN Member Nations)",
+    subtitle = "Percentage of children who received an immunization - horizontal line = 80%",
+    caption = "Data: nber.org \nGraphic: @weiyuet | #TidyTuesday2022 w29"
+  )
 
 # Save png
 ggsave("2022/w29/pol3-vaccinations-asean.png", width = 6, height = 8)
 
-technology %>% select(variable, label, group, category) %>% 
-  filter(group == "Consumption") %>% 
-  filter(category == "Communications") %>% 
+technology %>%
+  select(variable, label, group, category) %>%
+  filter(group == "Consumption") %>%
+  filter(category == "Communications") %>%
   distinct(variable)
 
 # Wrangle data
 # Cell subscribers in 10 ASEAN member nations
-cell_subsc_asean <- technology %>% 
-  filter(variable == "cell_subsc") %>% 
-  filter(iso3c == "SGP" | iso3c == "MYS" | iso3c == "IDN" | 
-           iso3c == "THA" | iso3c == "KHM" | iso3c == "LAO" | iso3c == "BRN" |
-           iso3c == "MMR" | iso3c == "PHL" | iso3c == "VNM")
+cell_subsc_asean <- technology %>%
+  filter(variable == "cell_subsc") %>%
+  filter(iso3c == "SGP" | iso3c == "MYS" | iso3c == "IDN" |
+    iso3c == "THA" | iso3c == "KHM" | iso3c == "LAO" | iso3c == "BRN" |
+    iso3c == "MMR" | iso3c == "PHL" | iso3c == "VNM")
 
 # Plot Cell subscribers in 10 ASEAN member nations
-cell_subsc_asean %>% 
+cell_subsc_asean %>%
   ggplot(aes(x = year, y = value, colour = iso3c)) +
   geom_line(size = 1.05, colour = "gray 35") +
   geom_hline(yintercept = 1000000, colour = "red", linetype = "dotted") +
@@ -75,24 +80,26 @@ cell_subsc_asean %>%
   scale_y_log10(labels = label_number(big.mark = ",")) +
   theme_classic() +
   theme(legend.position = "none") +
-  labs(x = "", y = "",
-       title = "Cell subscribers (10 ASEAN Member Nations)",
-       subtitle = "Log scale y-axis, and horizontal line at 1 million subscribers",
-       caption = "Source: nber.org \nGraphic: @weiyuet #TidyTuesday 2022 w29")
+  labs(
+    x = "", y = "",
+    title = "Cell subscribers (10 ASEAN Member Nations)",
+    subtitle = "Log scale y-axis, and horizontal line at 1 million subscribers",
+    caption = "Data: nber.org \nGraphic: @weiyuet | #TidyTuesday2022 w29"
+  )
 
 # Save png
 ggsave("2022/w29/cell-subscribers-asean.png", width = 6, height = 8)
 
 # Wrangle data
 # Internet users in 10 ASEAN member nations
-internetuser_asean <- technology %>% 
-  filter(variable == "internetuser") %>% 
-  filter(iso3c == "SGP" | iso3c == "MYS" | iso3c == "IDN" | 
-           iso3c == "THA" | iso3c == "KHM" | iso3c == "LAO" | iso3c == "BRN" |
-           iso3c == "MMR" | iso3c == "PHL" | iso3c == "VNM")
+internetuser_asean <- technology %>%
+  filter(variable == "internetuser") %>%
+  filter(iso3c == "SGP" | iso3c == "MYS" | iso3c == "IDN" |
+    iso3c == "THA" | iso3c == "KHM" | iso3c == "LAO" | iso3c == "BRN" |
+    iso3c == "MMR" | iso3c == "PHL" | iso3c == "VNM")
 
 # Plot Internet users in 10 ASEAN member nations
-internetuser_asean %>% 
+internetuser_asean %>%
   ggplot(aes(x = year, y = value, colour = iso3c)) +
   geom_line(size = 1.05, colour = "gray 35") +
   geom_hline(yintercept = 1000000, colour = "red", linetype = "dotted") +
@@ -100,24 +107,26 @@ internetuser_asean %>%
   scale_y_log10(labels = label_number(big.mark = ",")) +
   theme_classic() +
   theme(legend.position = "none") +
-  labs(x = "", y = "",
-       title = "Internet users (10 ASEAN member nations)",
-       subtitle = "Log scale y-axis, and horizontal line at 1 million users",
-       caption = "Source: nber.org \nGraphic: @weiyuet #TidyTuesday 2022 w29")
+  labs(
+    x = "", y = "",
+    title = "Internet users (10 ASEAN member nations)",
+    subtitle = "Log scale y-axis, and horizontal line at 1 million users",
+    caption = "Data: nber.org \nGraphic: @weiyuet | #TidyTuesday2022 w29"
+  )
 
 # Save png
 ggsave("2022/w29/internetuser-asean.png", width = 6, height = 8)
 
 # Wrangle data
 # Fixed telephones in 10 ASEAN member nations
-telephone_asean <- technology %>% 
-  filter(variable == "telephone") %>% 
-  filter(iso3c == "SGP" | iso3c == "MYS" | iso3c == "IDN" | 
-           iso3c == "THA" | iso3c == "KHM" | iso3c == "LAO" | iso3c == "BRN" |
-           iso3c == "MMR" | iso3c == "PHL" | iso3c == "VNM")
+telephone_asean <- technology %>%
+  filter(variable == "telephone") %>%
+  filter(iso3c == "SGP" | iso3c == "MYS" | iso3c == "IDN" |
+    iso3c == "THA" | iso3c == "KHM" | iso3c == "LAO" | iso3c == "BRN" |
+    iso3c == "MMR" | iso3c == "PHL" | iso3c == "VNM")
 
 # Plot Fixed telephones in 10 ASEAN member nations
-telephone_asean %>% 
+telephone_asean %>%
   ggplot(aes(x = year, y = value, colour = iso3c)) +
   geom_line(size = 1.05, colour = "gray 35") +
   geom_hline(yintercept = 100000, colour = "red", linetype = "dotted") +
@@ -125,10 +134,12 @@ telephone_asean %>%
   scale_y_log10(labels = label_number(big.mark = ",")) +
   theme_classic() +
   theme(legend.position = "none") +
-  labs(x = "", y = "",
-       title = "Fixed telephones (10 ASEAN member nations)",
-       subtitle = "Log scale y-axis, and horizontal line at 100,000 telephones",
-       caption = "Source: nber.org \nGraphic: @weiyuet #TidyTuesday 2022 w29")
+  labs(
+    x = "", y = "",
+    title = "Fixed telephones (10 ASEAN member nations)",
+    subtitle = "Log scale y-axis, and horizontal line at 100,000 telephones",
+    caption = "Data: nber.org \nGraphic: @weiyuet | #TidyTuesday2022 w29"
+  )
 
 # Save png
 ggsave("2022/w29/telephone-asean.png", width = 6, height = 8)
