cd2b56e8a4e9efe8b4154276cb6b9947027e18ca
weiyuet <weiyuet@gmail.com>
2022-09-28 17:46:30 +0800

    Updated w36 and w38. Imitate/inspired by mitsuoxv

--- 2022/w36/w36.R
+++ 2022/w36/w36.R
@@ -1,9 +1,51 @@
 # Setup
 library(tidyverse)
 library(scales)
-library(ggsci)
 
 # Load data
 inventories <- read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-09-06/inventories.csv.gz")
 inventory_sets <- read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-09-06/inventory_sets.csv.gz")
 sets <- read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-09-06/sets.csv.gz")
+
+# Merge inventory_sets and sets
+inventory_merged <- inventory_sets %>% left_join(sets, by = "set_num")
+
+inventory_merged %>% 
+  count(year, theme_id, wt = quantity, name = "quantity", sort = TRUE)
+
+# Plot past LEGO sets still in sale today
+range(inventory_merged$year)
+
+inventory_merged %>% 
+  count(set_num, name, year, wt = quantity, name = "quantity", sort = TRUE) %>% 
+  count(year, wt = quantity, name = "quantity") %>% 
+  ggplot(aes(x = year, y = quantity)) +
+  geom_col() +
+  scale_x_continuous(expand = c(0, 0),
+                     limits = c(1965, 2025),
+                     breaks = seq(1965, 2025, 10)) +
+  scale_y_continuous(expand = c(0, 0),
+                     limits = c(0, 600),
+                     breaks = seq(0, 600, 100)) +
+  theme_classic() +
+  labs(x = "Year of set debut", y = "Inventory quantity",
+       title = "LEGO Sets from 1964 are still in inventory",
+       caption = "Data: rebrickable\n#TidyTuesday2022 w36")
+  
+# Plot
+sets %>% 
+  filter(year >= 1970) %>% 
+  mutate(
+    decade = (year %/% 10) * 10,
+    decade = paste0(decade, "s")
+  ) %>% 
+  ggplot(aes(x = num_parts)) +
+  geom_density(adjust = 1/2, fill = "gray80") +
+  geom_vline(xintercept = 1000, linetype = 2) +
+  facet_wrap(~decade, nrow = 2) +
+  scale_x_log10() +
+  scale_y_continuous(expand = expansion(add = c(0, 0.1))) +
+  theme_classic() +
+  labs(x = "# of parts (log scale)", y = "",
+       title = "LEGO Sets with more than 1,000 parts are getting more popular",
+       caption = "Data: rebrickable\n#TidyTuesday2022 w36")
--- 2022/w38/w38.R
+++ 2022/w38/w38.R
@@ -10,8 +10,8 @@ hydro_waste <- read_csv('https://raw.githubusercontent.com/rfordatascience/tidyt
 # Clean column names
 hydro_waste <- hydro_waste %>% clean_names()
 
-# Plot data
-p <- map_data("world") %>% 
+# Plot areas with environmental concerns
+map_data("world") %>% 
   as_tibble() %>% 
   filter(region != "Antarctica") %>% 
   mutate(region = case_when(
@@ -28,9 +28,23 @@ p <- map_data("world") %>%
   scale_colour_manual(values = c("gray80", "red"),
                       guide = guide_legend(override.aes = list(size = 1))) +
   theme_minimal() +
-  theme(plot.background = element_rect(colour = "white", fill = "white")) +
+  theme(plot.background = element_rect(colour = "white", fill = "white"),
+        legend.position = c(0.15, 0.15)) +
   labs(x = "", y = "",
        title = "Waste Water Treatment Plants",
        subtitle = "Potential environmental concerns where plants show a dilution factor of less than 10",
        colour = "Dilution factor < 10?",
+       caption = "Data: Ehalt Macedo, H., Lehner, B., Nicell, J., Grill, G., Li, J., Limtong, A., and Shakya, R.:\nDistribution and characteristics of wastewater treatment plants within the global river network,\nEarth Syst. Sci. Data, 14, 559–577,\nhttps://doi.org/10.5194/essd-14-559-2022, 2022.")
+
+# Plot population served
+hydro_waste %>% 
+  ggplot(aes(pop_served)) +
+  geom_histogram(bins = 50) +
+  scale_x_log10(labels = label_number(big.mark = ","),
+                expand = c(0, 0)) +
+  scale_y_continuous(labels = label_number(big.mark = ","),
+                     expand = c(0, 0)) +
+  theme_minimal() +
+  labs(x = "Population served", y = "",
+       title = "Population Served by WWTP",
        caption = "Data: Ehalt Macedo, H., Lehner, B., Nicell, J., Grill, G., Li, J., Limtong, A., and Shakya, R.:\nDistribution and characteristics of wastewater treatment plants within the global river network,\nEarth Syst. Sci. Data, 14, 559–577,\nhttps://doi.org/10.5194/essd-14-559-2022, 2022.")
