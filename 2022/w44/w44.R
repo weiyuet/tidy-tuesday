a543572985a1450bf62eb8f8b9a774d1cd5e624d
weiyuet <weiyuet@gmail.com>
2022-11-30 18:01:55 +0800

    Added w44 and w45

--- 2022/w44/w44.R
+++ 2022/w44/w44.R
@@ -1,13 +1,18 @@
-# Setup
+###################
+# tidytuesday w44 #
+###################
+
+#### Setup ####
 library(tidyverse)
 library(scales)
 
-# Load data
+#### Load data ####
 horror_movies <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-11-01/horror_movies.csv')
 
+#### What is the relationship between revenue and budget ####
 horror_movies %>% 
   filter(genre_names == "Horror") %>% 
-  ggplot(aes(x = revenue, y = popularity, colour = genre_names)) +
+  ggplot(aes(x = revenue, y = budget, size = popularity, colour = genre_names)) +
   geom_point() +
   scale_x_log10(labels = label_number(prefix = "$",
                                       big.mark = ",")) +
@@ -16,6 +21,6 @@ horror_movies %>%
   theme(legend.position = "none") +
   labs(x = "", y = "",
        title = "Popularity Against Revenue of Horror Movies",
-       subtitle = "No apparent relationship between revenue and popularity,\nbut some movies had zero revenue, but are popular",
+       subtitle = "Some relationship between popularity and revenue",
        caption = "Data: www.themoviedb.org | #TidyTuesday2022 w44")
   
---
+++ 2022/w45/w45.R
@@ -0,0 +1,10 @@
+###################
+# tidytuesday w45 #
+###################
+
+#### Setup ####
+library(tidyverse)
+
+#### Load data ####
+state_stations <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-11-08/state_stations.csv')
+
