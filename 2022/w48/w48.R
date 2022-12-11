0b1c664e054f527a5e6a1d3389e367e84d292c48
weiyuet <weiyuet@gmail.com>
2022-12-11 11:44:34 +0800

    Added w48

--- 2022/w48/w48.R
+++ 2022/w48/w48.R
@@ -10,23 +10,26 @@ library(scales)
 wcmatches <- read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-11-29/wcmatches.csv')
 worldcups <- read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-11-29/worldcups.csv')
 
-#############
+##############################
 # How many goals are scored? #
-#############
+##############################
 
-#### Calculate goals per game and goals per team ####
+#### Calculate goals per game ####
 worldcups <- worldcups %>% 
-  mutate(goals_per_game = goals_scored/games,
-         goals_per_team = goals_scored/teams)
+  mutate(goals_per_game = goals_scored/games)
 
 #### Plot goals per game over time ####
 worldcups %>% 
   ggplot(aes(x = year, y = goals_per_game)) +
   geom_step() +
-  scale_x_continuous(breaks = seq(1930, 2018, 4))
+  scale_x_continuous(breaks = seq(1930, 2018, 8)) +
+  scale_y_continuous(limits = c(2, 6),
+                     breaks = seq(1, 6, 1)) +
+  theme_classic() +
+  labs(x = "", y = "Goals per game",
+       title = "Number of goals per game in the FIFA World Cup",
+       subtitle = "Goals scored per game is less than 3 in the modern era",
+       caption = "Data: Kaggle: FIFA World Cup\nGraphic: @weiyuet | #TidyTuesday2022 w48")
 
-#### Plot goals per game over time ####
-worldcups %>% 
-  ggplot(aes(x = year, y = goals_per_team)) +
-  geom_step() +
-  scale_x_continuous(breaks = seq(1930, 2018, 4))
+#### Save image ####
+ggsave("2022/w48/goals-per-game.png", width = 7, height = 5)
