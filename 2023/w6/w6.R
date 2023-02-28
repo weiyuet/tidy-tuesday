1555ffd4b00279394af9a10dba335fe0c9b5a979
weiyuet <weiyuet@gmail.com>
2023-02-28 23:30:25 +0800

    Edited w6

--- 2023/w6/w6.R
+++ 2023/w6/w6.R
@@ -6,17 +6,16 @@
 library(tidyverse)
 library(scales)
 library(paletteer)
-library(patchwork)
 
 #### Load data ####
 big_tech_stock_prices <- read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2023/2023-02-07/big_tech_stock_prices.csv')
 big_tech_companies <- read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2023/2023-02-07/big_tech_companies.csv')
 
 #### Visualize ####
-# FAANG stocks
-stocks_selected <- c("AAPL", "AMZN", "GOOGL", "META", "NFLX")
+# The fab 5
+stocks_selected <- c("AAPL", "AMZN", "GOOGL", "META", "MSFT")
 
-p1 <- big_tech_stock_prices %>%
+big_tech_stock_prices %>%
   filter(stock_symbol %in% stocks_selected) %>% 
   ggplot(aes(x = date,
              y = adj_close,
@@ -24,41 +23,15 @@ p1 <- big_tech_stock_prices %>%
   geom_step() +
   scale_x_date(labels = label_date_short(),
                date_breaks = "12 months") +
-  scale_y_log10(labels = label_number(prefix = "$",
-                                      big.mark = ""),
-                limits = c(1, 1000)) +
+  scale_y_continuous(labels = label_number(prefix = "$")) +
   scale_colour_paletteer_d("ggsci::default_jco") +
   labs(x = "",
        y = "",
-       title = "FAANG Stocks") +
+       title = "Big Tech Stock Crash 2022 - The Fab 5, is the party over?",
+       subtitle = "The Fab 5 generated 29.3% of the S&P500's gain by the end of the decade,\nbut accounted for 46.3% of the index's loss in 2022.",
+       caption = "Data: Yahoo Finance via Kaggle (by Evan Gower)\nGraphic: @weiyuet | #TidyTuesday2023 w6") +
   theme_classic() +
   theme(legend.title = element_blank())
-
-# non-FAANG stocks
-stocks_selected <- c("ADBE", "CRM", "CSCO", "IBM", "INTC", "MSFT", "NDVA", "ORCL", "TSLA")
-
-p2 <- big_tech_stock_prices %>%
-  filter(stock_symbol %in% stocks_selected) %>% 
-  ggplot(aes(x = date,
-             y = adj_close,
-             colour = stock_symbol)) +
-  geom_step() +
-  scale_x_date(labels = label_date_short(),
-               date_breaks = "12 months") +
-  scale_y_log10(labels = label_number(prefix = "$",
-                                      big.mark = ""),
-                limits = c(1, 1000)) +
-  scale_colour_paletteer_d("ggsci::default_jco") +
-  labs(x = "",
-       y = "",
-       title = "Non-FAANG Stocks") +
-  theme_classic() +
-  theme(legend.title = element_blank())
-
-p <- p1 / p2
-p + plot_annotation(title = "Big Tech Stock Crash of 2022",
-                    subtitle = "Plotted on a log scale, the 2022 crash is but a mere blip",
-                    caption = "Data: Yahoo Finance via Kaggle (by Evan Gower)\nGraphic: @weiyuet | TidyTuesday2023 w6")
 
 #### Save image ####
-ggsave("2023/w6/big-tech-stock-prices.png", width = 7, height = 7)
+ggsave("2023/w6/big-tech-stock-prices.png", width = 7, height = 5)
