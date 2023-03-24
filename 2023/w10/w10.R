d6785337f835c5da113445a941216afdee94777f
weiyuet <weiyuet@gmail.com>
2023-03-24 12:44:59 +0800

    Added w10

---
+++ 2023/w10/w10.R
@@ -0,0 +1,9 @@
+########################
+# TidyTuesday 2023 w10 #
+########################
+
+#### Setup ####
+library(tidyverse)
+
+#### Load data ####
+numbats <- read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2023/2023-03-07/numbats.csv')
