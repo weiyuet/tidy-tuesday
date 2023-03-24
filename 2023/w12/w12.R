7642bb3a21633aded13bb3ee37cdd253edc61c78
weiyuet <weiyuet@gmail.com>
2023-03-24 12:43:41 +0800

    Added w12

---
+++ 2023/w12/w12.R
@@ -0,0 +1,9 @@
+########################
+# TidyTuesday 2023 w12 #
+########################
+
+#### Setup ####
+library(tidyverse)
+
+#### Load data ####
+languages <- read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2023/2023-03-21/languages.csv')
