1dea30c891889bf24ae2c892378a8e2da6574907
weiyuet <weiyuet@gmail.com>
2023-05-13 19:37:33 +0800

    Edited code, added plot

--- 2023/w15/w15.R
+++ 2023/w15/w15.R
@@ -4,6 +4,8 @@
 
 #### Setup ####
 library(tidyverse)
+library(scales)
+library(paletteer)
 
 #### Load Data ####
 eggproduction  <- read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2023/2023-04-11/egg-production.csv')
@@ -19,11 +21,29 @@ eggproduction %>%
 range(eggproduction$observed_month) # Date from 2016 - 2021
 
 #### Visualize ####
-prod_process_selected <- c("cage-free (non-organic)", "cage-free (organic)")
+prod_process_selected <- c("cage-free (non-organic)",
+                           "cage-free (organic)")
 
 eggproduction %>% 
   filter(prod_process %in% prod_process_selected) %>% 
   ggplot(aes(x = observed_month,
              y = n_eggs,
              colour = prod_process)) +
-  geom_line()
+  geom_line() +
+  scale_x_date(date_breaks = "3 months",
+               labels = label_date_short()) +
+  scale_y_continuous(labels = label_number(big.mark = ",")) +
+  scale_colour_paletteer_d("ggsci::default_jco") +
+  labs(x = "",
+       y = "",
+       colour = "",
+       title = "Egg Production in the USA (2016-2021)",
+       subtitle = "The production of non-organic eggs still far outpaces organic eggs.",
+       caption = "Data: US Egg Production Data\nGraphic: @weiyuet | #TidyTuesday2023 w15") +
+  theme_classic() +
+  theme(legend.position = c(0.2, 0.85))
+
+#### Save Image ####
+ggsave("2023/w15/egg-production.png",
+       width = 7,
+       height = 5)
